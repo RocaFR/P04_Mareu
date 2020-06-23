6546f5d7219b9598ac6292410c2561d4ef10d546
Bryan F.R <bryan.ferreras@gmail.com>
2020-06-23 16:26:05 +0200

    Implementation of the Meeting Rooms list.

--- app/src/main/java/bryan/roca/mareu/service/DummyMeetingGenerator.java
+++ app/src/main/java/bryan/roca/mareu/service/DummyMeetingGenerator.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import bryan.roca.mareu.models.Meeting;
+import bryan.roca.mareu.models.MeetingRoom;
 
 /**
  * Mareu - bryan.roca.mareu.service
@@ -15,8 +16,23 @@ import bryan.roca.mareu.models.Meeting;
 public class DummyMeetingGenerator {
 
     public static List<Meeting> DUMMY_MEETINGS = Arrays.asList();
+    public static List<MeetingRoom> DUMMY_MEETINGROOMS = Arrays.asList(
+            new MeetingRoom("Dune du Pilat"),
+            new MeetingRoom("L'Ile aux oiseaux"),
+            new MeetingRoom("Banc d'Arguin"),
+            new MeetingRoom("Banc du Toulinguet"),
+            new MeetingRoom("Le Warf"),
+            new MeetingRoom("Peireire"),
+            new MeetingRoom("Phare du Cap Ferret"),
+            new MeetingRoom("Cabanes tchanquées"),
+            new MeetingRoom("Pinasse"),
+            new MeetingRoom("You le phoque")
+    );
     
     public static List<Meeting> generateMeetings() {
         return new ArrayList<>(DUMMY_MEETINGS);
+    }
+    public static List<MeetingRoom> generateMeetingRooms() {
+        return new ArrayList<>(DUMMY_MEETINGROOMS);
     }
 }
