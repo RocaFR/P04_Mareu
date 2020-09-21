9c9d59bea901f427ec34f5d979ff6ac34a24a5ef
Bryan F.R <bryan.ferreras@gmail.com>
2020-09-21 12:04:49 +0200

    Implement the android test canWeFilterMeetingByDate()

--- app/src/androidTest/java/bryan/roca/mareu/LogicalInstrumentedTest.java
+++ app/src/androidTest/java/bryan/roca/mareu/LogicalInstrumentedTest.java
@@ -1,28 +1,44 @@
 package bryan.roca.mareu;
 
-import android.app.AlertDialog;
-import android.app.DatePickerDialog;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.PickerActions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.joda.time.DateTime;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import bryan.roca.mareu.controllers.activities.MainActivity;
+import bryan.roca.mareu.controllers.fragments.DatePickerFragment;
+import bryan.roca.mareu.utils.RecyclerViewMatcher;
+import bryan.roca.mareu.utils.RemoveMeetingAction;
 
 import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
@@ -34,9 +50,14 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class LogicalInstrumentedTest {
 
-    private String name = "Instrumental Test";
-    private String email = "bryan.ferreras@gmail.com";
-    //TODO Use API to add Meetings to the list for "canWeSeeMeetingList", "canWeFilterMeetingByMeetingRoom" and "canWeRemoveMeeting"
+    private String firstMeetingName = "Instrumental Test";
+    private String secondMeetingName = "Another Meeting";
+    private String firstMeetingEmail = "bryan.ferreras@gmail.com";
+    private String secondMeetingEmail = "moussion.solene@gmail.com";
+    private static final String BEGIN_DATE_FIRST_MEETING = "01/01/2020";
+    private static final String END_DATE_FIRST_MEETING = "01/01/2020";
+    private static final String BEGIN_DATE_SECOND_MEETING = "02/01/2020";
+    private static final String END_DATE_SECOND_MEETING = "02/01/2020";
 
     /**
      * Rule for launching activity
@@ -44,7 +65,6 @@ public class LogicalInstrumentedTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule(MainActivity.class);
-
 
     @Test
     public void useAppContext() {
@@ -63,57 +83,143 @@ public class LogicalInstrumentedTest {
     }
 
     /**
-     * Ensure we can add a Meeting correctly
+     * Ensure the RecyclerView display the added Meeting list
      */
     @Test
-    public void canWeAddMeeting() {
+    public void canWeAddAndSeeMeetingList() {
         onView(withId(R.id.floatingButton_addMeeting))
                 .perform(click());
         onView(withId(R.id.editText_addMeeting_activity_meetingName))
-                .perform(typeText(name));
+                .perform(typeText(firstMeetingName));
         onView(withId(R.id.imageButton_addMeeting_activity_addParticipant))
                 .perform(scrollTo(), click());
         onView(withId(R.id.fragment_add_participant_editText_mail_address))
-                .perform(typeText(email));
+                .perform(typeText(firstMeetingEmail));
         onView(withText(R.string.alert_dialog_positive_button))
                 .perform(click());
         onView(withId(R.id.imageButton_addMeeting_activity_addMeeting))
                 .perform(scrollTo(), click());
-    }
 
-    /**
-     * Ensure the RecyclerView display the Meeting list
-     */
-    @Test
-    public void canWeSeeMeetingList() {
+        onView(withRecyclerView(R.id.recyclerView).atPosition(0))
+                .check(matches(hasDescendant(withText(firstMeetingName))));
     }
 
     @Test
     public void canWeFilterMeetingByDate() {
+        // First Meeting
+        onView(withId(R.id.floatingButton_addMeeting))
+                .perform(click());
+        onView(withId(R.id.editText_addMeeting_activity_meetingName))
+                .perform(typeText(firstMeetingName));
+        onView(withId(R.id.textView_addMeeting_activity_dateBegin))
+                .perform(setTextInTextView(BEGIN_DATE_FIRST_MEETING));
+        onView(withId(R.id.textView_addMeeting_activity_dateEnd))
+                .perform(scrollTo(), setTextInTextView(END_DATE_FIRST_MEETING));
+        onView(withId(R.id.imageButton_addMeeting_activity_addParticipant))
+                .perform(scrollTo(), click());
+        onView(withId(R.id.fragment_add_participant_editText_mail_address))
+                .perform(typeText(firstMeetingEmail));
+        onView(withText(R.string.alert_dialog_positive_button))
+                .perform(click());
+        onView(withId(R.id.imageButton_addMeeting_activity_addMeeting))
+                .perform(scrollTo(), click());
+
+        // Second Meeting
+        onView(withId(R.id.floatingButton_addMeeting))
+                .perform(click());
+        onView(withId(R.id.editText_addMeeting_activity_meetingName))
+                .perform(typeText(secondMeetingName));
+        onView(withId(R.id.textView_addMeeting_activity_dateBegin))
+                .perform(scrollTo(), setTextInTextView(BEGIN_DATE_SECOND_MEETING));
+        onView(withId(R.id.textView_addMeeting_activity_dateEnd))
+                .perform(scrollTo(), setTextInTextView(END_DATE_SECOND_MEETING));
+        onView(withId(R.id.imageButton_addMeeting_activity_addParticipant))
+                .perform(scrollTo(), click());
+        onView(withId(R.id.fragment_add_participant_editText_mail_address))
+                .perform(typeText(firstMeetingEmail));
+        onView(withText(R.string.alert_dialog_positive_button))
+                .perform(click());
+        onView(withId(R.id.imageButton_addMeeting_activity_addMeeting))
+                .perform(scrollTo(), click());
+
         onView(withId(R.id.menuMain_item_filter))
                 .perform(click());
         // Date begin
         onView(withId(R.id.fragment_filter_textView_dateBegin))
-                .perform(click());
-        onView(withText("OK"))
-                .perform(click());
+                .perform(setTextInTextView(BEGIN_DATE_SECOND_MEETING));
         // Date end
         onView(withId(R.id.fragment_filter_textView_dateEnd))
-                .perform(click());
-        onView(withText("OK"))
-                .perform(click());
+                .perform(setTextInTextView(END_DATE_SECOND_MEETING));
         // Filter
         onView(withText(MainActivity.ALERTDIALOG_FILTER_POSITIVE_TEXT))
                 .perform(click());
+        // Assert that the Meeting displayed is the good one
+        onView(withRecyclerView(R.id.recyclerView).atPosition(0))
+                .check(matches(not(hasDescendant(withText(BEGIN_DATE_FIRST_MEETING)))));
+
     }
 
     @Test
     public void canWeFilterMeetingByMeetingRoom() {
-
+        //TODO Comment sélectionner un élément dans un spinner
     }
 
     @Test
     public void canWeRemoveMeeting() {
-        
+        onView(withId(R.id.floatingButton_addMeeting))
+                .perform(click());
+        onView(withId(R.id.editText_addMeeting_activity_meetingName))
+                .perform(typeText(firstMeetingName));
+        onView(withId(R.id.imageButton_addMeeting_activity_addParticipant))
+                .perform(scrollTo(), click());
+        onView(withId(R.id.fragment_add_participant_editText_mail_address))
+                .perform(typeText(firstMeetingEmail));
+        onView(withText(R.string.alert_dialog_positive_button))
+                .perform(click());
+        onView(withId(R.id.imageButton_addMeeting_activity_addMeeting))
+                .perform(scrollTo(), click());
+
+        onView(ViewMatchers.withId(R.id.recyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, new RemoveMeetingAction()));
+        onView(withId(R.id.recyclerView))
+                .check(matches(not(withText(firstMeetingName))));
+    }
+
+    //
+    // UTILS FOR TESTS
+    //
+
+    /**
+     * Util for perfom Recyclerview actions
+     * @param pRecyclerViewId
+     * @return
+     */
+    private static RecyclerViewMatcher withRecyclerView(final int pRecyclerViewId) {
+        return new RecyclerViewMatcher(pRecyclerViewId);
+    }
+
+    /**
+     * Useful for assign value to a TextView
+     * @param pValue
+     * @return
+     */
+    private static ViewAction setTextInTextView(final String pValue) {
+        return new ViewAction() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public Matcher<View> getConstraints() {
+                return allOf(isDisplayed(), isAssignableFrom(TextView.class));
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((TextView) view).setText(pValue);
+            }
+
+            @Override
+            public String getDescription() {
+                return "replace text";
+            }
+        };
     }
 }
