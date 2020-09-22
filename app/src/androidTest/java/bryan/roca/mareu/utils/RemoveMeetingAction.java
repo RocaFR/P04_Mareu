fc0453387934c51afda588ec5d13253c9a132991
Bryan F.R <bryan.ferreras@gmail.com>
2020-09-22 20:15:59 +0200

    Utils for Android test

---
+++ app/src/androidTest/java/bryan/roca/mareu/utils/RemoveMeetingAction.java
@@ -0,0 +1,27 @@
+package bryan.roca.mareu.utils;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+import bryan.roca.mareu.R;
+
+public class RemoveMeetingAction implements ViewAction {
+    @Override
+    public Matcher<View> getConstraints() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public void perform(UiController uiController, View view) {
+        View button = view.findViewById(R.id.imageButton_removeMeeting);
+        button.performClick();
+    }
+}
